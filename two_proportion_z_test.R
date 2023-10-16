4302b17a86b24895af8ad414f4fab1a8905060ee
jbutler93 <131003548+jbutler93@users.noreply.github.com>
2023-10-16 17:32:06 +0100

    Update

--- two_proportion_z_test.R
+++ two_proportion_z_test.R
@@ -1,16 +1,45 @@
-# Example data
+### Load Excel ###
+
+library(readxl)
+library(dplyr)
+
+responses <-  read_excel("C:/Users/m1012129/Downloads/responses.xlsx")
+
+
+### find the latest data ###
+
+latest <- responses %>%
+  filter(time == "April 2023")
+
+previous <- responses %>%
+  filter(time == "October 2022")
+
+answer1 = "Yes, I fully understand Defra's vision for farming"
+  
+### Data ###
+  
+latest_question <- latest %>%
+  filter(answer == answer1) # create a table of the latest time
+
+previous_question <- previous %>%
+  filter(answer == answer1) # create a table of the latest time
+
+time_values <- unique(responses$time) # find all unique time names
+
 
 # Group 1
-n1 <- 1418 # Sample size for group 1
-x1 <- 142  # Number of successes in group 1
+n1 <- latest_question$total # Sample size for group 1
+x1 <- latest_question$number  # Number of successes in group 1
 
 # Group 2
-n2 <- 1223 # Sample size for group 2
-x2 <- 86  # Number of successes in group 2
+n2 <- previous_question$total # Sample size for group 2
+x2 <- previous_question$number  # Number of successes in group 2
 
 # Performing the two-proportion z-test
 # H0: p1 = p2 (the proportions are equal)
 # H1: p1 ≠ p2 (the proportions are not equal)
+
+### Manipulation ###
 
 # Step 1: Compute the sample proportions
 p1 <- x1 / n1
@@ -31,12 +60,18 @@ alpha <- 0.05
 # Step 6: Find the critical value for the test statistic
 critical_value <- qnorm(1 - alpha / 2)
 
+### Results ###
+
 # Step 7: Compare the test statistic with the critical value
 if (abs(z) > critical_value) {
   cat("Reject the null hypothesis. There is a significant difference between the proportions.\n")
 } else {
   cat("Fail to reject the null hypothesis. There is no significant difference between the proportions.\n")
 }
+
+# Step 8: Calculate the p-value
+p_value <- 2 * (1 - pnorm(abs(z)))
+cat("The p-value is:", p_value, "\n")
 
 # Step 8: Calculate the p-value
 p_value <- 2 * (1 - pnorm(abs(z)))
